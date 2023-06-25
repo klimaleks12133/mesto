9748b1c5537f8555c684bf792a827013561014ec
klimaleks12133 <120417478+klimaleks12133@users.noreply.github.com>
2023-06-25 13:30:33 +0300

    popup

--- scripts/script.js
+++ scripts/script.js
@@ -1,33 +1,31 @@
-const popup = document.querySelector('.popup');                              // Находим блок с формой
-const popupForm = document.querySelector('.popup__container');               // Находим форму
-const popupOpenButton = document.querySelector('.profile__edit-button');     // Находим кнопку редактирования профиля
-const popupCloseButton = popup.querySelector('.popup__close-button');        // Находим кнопку закрытия попапа
-const popupSaveButton = popup.querySelector('.popup__save-button');          // Находим кнопку сохранения
-const nameInput = popupForm.querySelector('.popup__input[name="name"]');      // Находим инпут с именем
-const jobInput = popupForm.querySelector('.popup__input[name="job"]');        // Находим инпут с профессией
-const profileName = document.querySelector('.profile__name');                // Находим элемент для вставки значений
-const profileJob = document.querySelector('.profile__job');                  // Находим элемент для вставки значений
-
-const popupToggle = function () {
-  if (!popup.classList.contains('popup_opened')) {                           // Функция добавления / удаления класса видимости у попапа
-
-    nameInput.value = profileName.textContent;                                  // Помещаем Имя в инпут формы
-    jobInput.value = profileJob.textContent;                                    // Помещаем Профессию в инпут формы
-    popup.classList.toggle('popup_opened');
-
-  } else {
-    popup.classList.toggle('popup_opened');
-  }
+const content = document.querySelector(".content");
+const popup = document.querySelector(".popup");
+const profileEdit = content.querySelector(".profile__edit-button");
+const popupButtonClose = document.querySelector(".popup__close");
+const profileTitle = content.querySelector(".profile__usermane");
+const profileSubtitle = content.querySelector(".profile__about");
+const popupForm = document.querySelector(".popup__form");
+const nameInput = document.querySelector(".popup__input_username");
+const jobInput = document.querySelector("popup__input_job");
+// ВАЖНО! Я не понимаю почему код не работает, если поможете, буду благодарен )
+function openProfileButton() {
+  popup.classList.add("popup_opened");
+  nameInput.value = profileTitle.textContent;
+  jobInput.value = profileSubtitle.textContent;
 }
 
-function formSubmitHandler(evt) {
-  evt.preventDefault();                                                      // Отменяем стандартное поведение формы
-  // Вставляем значения инпутов в выбранные элементы
-  profileName.textContent = nameInput.value;
-  profileJob.textContent = jobInput.value;
-  popupToggle();                                                             // Закрываем попап
+function closePopupButton () {
+  popup.classList.remove("popup_opened");
 }
-// Слушатели
-popupForm.addEventListener('submit', formSubmitHandler);                     // Добавляем слушателя кнопке сохранения 
-popupOpenButton.addEventListener('click', popupToggle);                      // Добавляем слушателя кнопке редактирования профиля
-popupCloseButton.addEventListener('click', popupToggle);                     // Добавляем слушателя кнопке закрытия попапа
+
+function handleFormSubmit(evt) {
+  evt.preventDefault();
+  profileTitle.textContent = nameInput.value;
+  profileSubtitle.textContent = jobInput.value;
+  closePopupButton();
+}
+
+popupForm.addEventListener("submit", handleFormSubmit);
+
+openProfileButton.addEventListener("click", openProfileButton);
+popupButtonClose.addEventListener("click", closePopupButton);
