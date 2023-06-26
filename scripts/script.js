d4d339506afaf43e7beef676fcfe8d2dc8950305
klimaleks12133 <120417478+klimaleks12133@users.noreply.github.com>
2023-06-26 14:29:10 +0300

    fix: исправлены замечания ревьера

--- scripts/script.js
+++ scripts/script.js
@@ -5,9 +5,9 @@ const popupButtonClose = document.querySelector(".popup__close");
 const profileTitle = content.querySelector(".profile__usermane");
 const profileSubtitle = content.querySelector(".profile__about");
 const popupForm = document.querySelector(".popup__form");
-const nameInput = document.querySelector(".popup__input_username");
-const jobInput = document.querySelector(".popup__input_job");
-// ВАЖНО! Я не понимаю почему код не работает, если поможете, буду благодарен )
+const nameInput = document.querySelector(".popup__input_type_username");
+const jobInput = document.querySelector(".popup__input_type_job");
+
 function openProfileButton() {
   popup.classList.add("popup_opened");
   nameInput.value = profileTitle.textContent;
