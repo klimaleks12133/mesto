0beab49813fe493173a7db858c6c2558b9f2a5b6
klimaleks12133 <120417478+klimaleks12133@users.noreply.github.com>
2023-06-26 10:58:46 +0300

    fix: popup

--- scripts/script.js
+++ scripts/script.js
@@ -1,12 +1,12 @@
 const content = document.querySelector(".content");
 const popup = document.querySelector(".popup");
-const profileEdit = content.querySelector(".profile__edit-button");
+const profileButton = content.querySelector(".profile__edit-button");
 const popupButtonClose = document.querySelector(".popup__close");
 const profileTitle = content.querySelector(".profile__usermane");
 const profileSubtitle = content.querySelector(".profile__about");
 const popupForm = document.querySelector(".popup__form");
 const nameInput = document.querySelector(".popup__input_username");
-const jobInput = document.querySelector("popup__input_job");
+const jobInput = document.querySelector(".popup__input_job");
 // ВАЖНО! Я не понимаю почему код не работает, если поможете, буду благодарен )
 function openProfileButton() {
   popup.classList.add("popup_opened");
