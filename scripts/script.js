b3a5f7555b70d336290cc1064a11d9f38fdeeb19
klimaleks12133 <120417478+klimaleks12133@users.noreply.github.com>
2023-07-03 19:08:05 +0300

    feat: добавление popup-add и значка удалить

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,37 @@
+const initialCards = [
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+];
+
 const content = document.querySelector(".content");
 const popup = document.querySelector(".popup");
+const popupAdd = document.querySelector(".popup-add");
 const profileButton = content.querySelector(".profile__edit-button");
+const profileAddButton = content.querySelector(".profile__add-button");
 const popupButtonClose = document.querySelector(".popup__close");
+const closeAddButton = document.querySelector(".popup-add__close");
 const profileTitle = content.querySelector(".profile__usermane");
 const profileSubtitle = content.querySelector(".profile__about");
 const popupForm = document.querySelector(".popup__form");
@@ -14,7 +44,15 @@ function openProfileButton() {
   jobInput.value = profileSubtitle.textContent;
 }
 
-function closePopupButton () {
+function openPopupAdd() {
+  popupAdd.classList.add('popup-add_opened');
+}
+
+function closePopupAdd() {
+  popupAdd.classList.remove('popup-add_opened');
+}
+
+function closePopupButton() {
   popup.classList.remove("popup_opened");
 }
 
@@ -28,3 +66,5 @@ function handleFormSubmit(evt) {
 popupForm.addEventListener("submit", handleFormSubmit);
 profileButton.addEventListener("click", openProfileButton);
 popupButtonClose.addEventListener("click", closePopupButton);
+profileAddButton.addEventListener('click', openPopupAdd);
+closeAddButton.addEventListener('click', closePopupAdd);
