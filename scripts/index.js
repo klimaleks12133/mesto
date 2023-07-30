3c77d2b2c6b4fc310debeda7a1d0be7e5e0d6d9d
klimaleks12133 <120417478+klimaleks12133@users.noreply.github.com>
2023-07-30 17:32:56 +0300

    refactor: add class FormValidator  and class initialCards

--- scripts/index.js
+++ scripts/index.js
@@ -21,37 +21,7 @@ const popupImageCaption = popupImage.querySelector('.popup__caption');
 const popupImagePhoto = popupImage.querySelector('.popup__photo');
 const cardFormSubmitButton = formAdd.querySelector('.popup__form-submit');
 
-
-
-const initialCards = [
-  {
-      name: 'Архыз',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
-  },
-  {
-      name: 'Челябинская область',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
-  },
-  {
-      name: 'Иваново',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
-  },
-  {
-      name: 'Камчатка',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
-
-  },
-  {
-      name: 'Холмогорский район',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
-
-  },
-  {
-      name: 'Байкал',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
-
-  }
-];
+import { initialCards } from '../scripts/initialCards.js';
 
 const config = {
     formSelector: '.popup__form',
